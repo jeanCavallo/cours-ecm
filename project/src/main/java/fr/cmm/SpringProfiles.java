33e9759a8d42380eff7c5da17d4e1b2cda0ecf63
damien bourdette <dbourdette@gmail.com>
2015-07-14 09:08:01 +0200

    Publish of Github pages from Gradle.

--- project/src/main/java/fr/cmm/SpringProfiles.java
+++
@@ -1,9 +0,0 @@
-package fr.cmm;
-
-public class SpringProfiles {
-    public static final String TEST = "test";
-
-    public static final String INTEG = "integ";
-
-    public static final String PROD = "prod";
-}
